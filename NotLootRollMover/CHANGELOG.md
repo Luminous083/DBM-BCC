85e7fd9fe98b5aa12b0f77f4d98ca6d32b9a0cf0
Luminous083 <105128465+Luminous083@users.noreply.github.com>
2022-05-12 00:38:01 -0400

    update

---
+++ NotLootRollMover/CHANGELOG.md
@@ -0,0 +1,6 @@
+# LootRollMover
+
+## [v4.7](https://github.com/Xruptor/LootRollMover/tree/v4.7) (2021-09-19)
+[Full Changelog](https://github.com/Xruptor/LootRollMover/compare/v4.6...v4.7) [Previous Releases](https://github.com/Xruptor/LootRollMover/releases)
+
+- TOC Version Update  
---
+++ WeakAurasCompanion/data.lua
@@ -0,0 +1,23 @@
+-- file generated automatically
+WeakAurasCompanion = {
+  WeakAuras = {
+    slugs = {
+    },
+    uids = {
+    },
+    ids = {
+    },
+    stash = {
+    },
+  },
+  Plater = {
+    slugs = {
+    },
+    uids = {
+    },
+    ids = {
+    },
+    stash = {
+    },
+  },
+}
